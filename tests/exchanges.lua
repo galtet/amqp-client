6119eff6f44c2ad1c0a0b23785b119fcc63aaa26
galt <galt@il.ibm.com>
2020-10-05 01:13:41 +0300

    adding exchange tests

---
+++ tests/exchanges.lua
@@ -0,0 +1,33 @@
+local amqp = require('amqp')
+
+local conn = amqp.new({
+  host = '127.0.0.1',
+  port = 5672,
+  username = "admin",
+  password = "admin",
+  vhost ="/"
+})
+
+local msg = "this is a test"
+local route_key = "test_route_key"
+local exchange_name = "test_ex_1"
+local queue_name = "test_queue_1"
+
+local channel = conn:open_channel()
+local queue = channel:queue_declare(queue_name)
+local exchange = channel:exchange_declare(exchange_name)
+
+queue:bind(exchange_name, route_key)
+exchange:publish_message(route_key, msg)
+
+local new_msg, tag = queue:consume_message()
+
+if (msg ~= new_msg) then
+  error("test failed ! wrong msg:" .. new_msg)
+end
+
+-- acking the msg
+channel:ack(tag)
+
+-- close the connection
+conn:close()
