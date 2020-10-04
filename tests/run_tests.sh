db4c839a3b0cf25abcd8598338c871a3ac528cf6
galt <galt@il.ibm.com>
2020-10-05 00:39:31 +0300

    adding waiting for port

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 # waiting for rabbitmq
-sleep 10
+timeout 20 sh -c 'until nc -z $0 $1; do sleep 1; done' rabbitmq 5672
 
 for f in $(find ./tests/ -type f -name "*.lua"); do
   echo "Running js test: $f"
