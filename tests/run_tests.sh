c9014e93f6cf037e30e2be71cc676ea5544857d3
galt <galt@il.ibm.com>
2020-10-08 00:46:10 +0300

    fix valgrind

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,5 +6,5 @@ timeout 20 sh -c 'until nc -z $0 $1; do sleep 1; done' rabbitmq 5672
 
 for f in $(find ./tests/ -type f -name "*.lua"); do
   echo "Running js test: $f"
-  valgrind luajit --undef-value-errors=no --error-exitcode=1 $f
+  valgrind --undef-value-errors=no --error-exitcode=1 luajit $f
 done
